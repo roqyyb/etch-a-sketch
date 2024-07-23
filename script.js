6b4c5766fa11f471cfeb59c954b1f3ef9a1bf52d
ROQYYB <125754105+roqyyb@users.noreply.github.com>
2024-07-23 11:03:06 +0100

    Implement logic to color a square onmouseover event

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const container = document.querySelector("div");
 
-function etchASketch(n = 9) {
+function etchASketch(n = 10) {
   for (let i = 1; i <= n ** 2; ++i) {
     const square = document.createElement("div");
     square.setAttribute("class", "square");
+    square.setAttribute("id", `square-${i}`);
     square.style.width = `${800 / n - 2}px`;
     square.style.height = `${800 / n - 2}px`;
     container.appendChild(square);
+
+    // color square on mouseover
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "green";
+      console.log(square.id);
+    });
   }
 }
 
