71f21027f07ccb9b7059380f4efe50253feffacf
ROQYYB <125754105+roqyyb@users.noreply.github.com>
2024-07-23 02:21:05 +0100

    Implement a logic to create an n by n  grid

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("working");
+const container = document.querySelector("div");
+
+function etchASketch(n = 9) {
+  for (let i = 1; i <= n ** 2; ++i) {
+    const square = document.createElement("div");
+    square.setAttribute("class", "square");
+    square.style.width = `${800 / n - 2}px`;
+    square.style.height = `${800 / n - 2}px`;
+    container.appendChild(square);
+  }
+}
+
+etchASketch();
